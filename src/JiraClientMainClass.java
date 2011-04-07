dd696bce5765ede196b1a94b8b3524edd6f03156
Ivan Sphinks <sphinks@newmail.ru>
2011-04-07 17:15:52 +0400

    Last on 7 of April 2011

--- src/JiraClientMainClass.java
+++ src/JiraClientMainClass.java
@@ -1,3 +1,12 @@
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import com.atlassian.jira.rest.client.JiraRestClient;
+import com.atlassian.jira.rest.client.JiraRestClientFactory;
+import com.atlassian.jira.rest.client.NullProgressMonitor;
+import com.atlassian.jira.rest.client.domain.Issue;
+import com.atlassian.jira.rest.client.internal.jersey.JerseyJiraRestClientFactory;
+
 
 public class JiraClientMainClass {
 
@@ -6,6 +15,19 @@ public class JiraClientMainClass {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		final JerseyJiraRestClientFactory factory = new JerseyJiraRestClientFactory();
+		try{
+			final URI jiraServerUri = new URI("http://sandbox.onjira.com:8089/jira");
+			final JiraRestClient restClient = factory.createWithBasicHttpAuthentication(jiraServerUri, "sphinks", "654321");
+			final NullProgressMonitor pm = new NullProgressMonitor();
+			final Issue issue = restClient.getIssueClient().getIssue("TST-1", pm);
+
+			System.out.println(issue);
+		}catch(URISyntaxException ex){
+			
+		}
+		
 
 	}
 
