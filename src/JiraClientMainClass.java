60080c024abe326edd3c44bec6106dcc05a320e5
Ivan Sphinks <sphinks@newmail.ru>
2011-04-08 17:58:32 +0400

    8th April

--- src/JiraClientMainClass.java
+++ src/JiraClientMainClass.java
@@ -2,7 +2,6 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import com.atlassian.jira.rest.client.JiraRestClient;
-import com.atlassian.jira.rest.client.JiraRestClientFactory;
 import com.atlassian.jira.rest.client.NullProgressMonitor;
 import com.atlassian.jira.rest.client.domain.Issue;
 import com.atlassian.jira.rest.client.internal.jersey.JerseyJiraRestClientFactory;
@@ -14,20 +13,26 @@ public class JiraClientMainClass {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		URI jiraServerUri = null;
+		JiraRestClient restClient = null;
+		final NullProgressMonitor pm = new NullProgressMonitor();
 		
 		final JerseyJiraRestClientFactory factory = new JerseyJiraRestClientFactory();
 		try{
-			final URI jiraServerUri = new URI("http://sandbox.onjira.com");
-			final JiraRestClient restClient = factory.createWithBasicHttpAuthentication(jiraServerUri, "sphinks", "654321");
-			final NullProgressMonitor pm = new NullProgressMonitor();
-			final Issue issue = restClient.getIssueClient().getIssue("TST-1", pm);
-
-			System.out.println(issue);
-		}catch(URISyntaxException ex){
+			jiraServerUri = new URI("http://sandbox.onjira.com");
+			restClient = factory.createWithBasicHttpAuthentication(jiraServerUri, "sphinks", "654321");
 			
+
+			
+		}catch(URISyntaxException ex){
+			System.out.println("Can`t establish connection with " + jiraServerUri.toString());
 		}
-		
+		CommandParcer cp = new CommandParcer();
+		Command com = cp.parseCommand(args[0]);
+		if (com.equals(new Command(CommandsList.issue,"",""))) {
+			final Issue issue = restClient.getIssueClient().getIssue("TST-1", pm);
+			System.out.println(issue);
+		}
 
 	}
 
