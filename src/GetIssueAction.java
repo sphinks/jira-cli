1bb6e9eacddaa5052e5c0504acd88724c07a130a
Ivan Sphinks <sphinks@newmail.ru>
2011-07-20 12:05:15 +0400

    Add jira-rest libe v 0.3

--- src/GetIssueAction.java
+++ src/GetIssueAction.java
@@ -2,6 +2,7 @@ import org.apache.commons.cli.Option;
 
 import com.atlassian.jira.rest.client.JiraRestClient;
 import com.atlassian.jira.rest.client.domain.Issue;
+import com.atlassian.jira.rest.client.domain.TimeTracking;
 
 /**
  * @author Zerin_IS
@@ -21,7 +22,11 @@ public class GetIssueAction extends CommandAction {
 		
 		if (arguments.length > 0) {
 			issue = restClient.getIssueClient().getIssue(arguments[0], pm);
-			result = "Issue: " + issue.getKey() + " (" + issue.getSelf().toString() + ")\r\n" + "Summary: " + issue.getSummary();
+			result = "Issue: " + issue.getKey() + " (" + issue.getSelf().toString() + ")\r\n" + "Summary: " + issue.getSummary() + "\r\n";
+			
+			result += "\r\n" + "Time for issue: " + issue.getTimeTracking().getOriginalEstimateMinutes() + " min";
+			result += "\r\n" + "Time for left issue: " + issue.getTimeTracking().getRemainingEstimateMinutes() + " min";
+			
 			return result;
 		}else{
 			result = "Error: specify name of looking issue.";
